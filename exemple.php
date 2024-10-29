40b3682d93c2ca35ff5f9f0c4dc4b5036752584f
Laurent Giustignano <laurent.giustignano@free.fr>
2024-10-29 12:00:02 +0100

    R3.01-B-TP0.2 variables

--- exemple.php
+++ exemple.php
@@ -1,5 +1,16 @@
-Bonjour à tous !
 <?php
-echo "Comment ça va ?";
-?>
+/* Les nombres */
+$nombreEntier = 12;
+$nombreFlottant = 3.4;
+
+/* Les chaines de caractères */
+$chaine = "comme ceci";
+$autreChaine = 'ou comme cela...';
+
+$unBooleen = true; // ou bien false
+
+/* une constante */
+const PI = 3.14;
+
+
 
